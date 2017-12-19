671535e7c1d6b6ec1c19c2f044002fe0c940ee48
Michael Ciocca <mciocca89@gmail.com>
2017-12-19 17:00:31 -0500

    ended on page 55

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,15 +1,57 @@
 import Ember from 'ember';
 
 const Band = Ember.Object.extend({
-    name: ''
+  name: ''
+});
+
+var Song = Ember.Object.extend({
+  title: '',
+  rating: 0,
+  band: ''
 });
 
 export default Ember.Route.extend({
-    model(){
-        const ledZepplin = Band.create({ name: 'Led Zepplin' });
-        const pearlJam = Band.create({ name: 'Pearl Jam' });
-        const fooFighters = Band.create({ name: 'Foo Fighter' });
+  model() {
+    
+    var blackDog = Song.create({
+      title: 'Black Dog',
+      band: 'Led Zeppelin',
+      rating: 3
+    });
 
-        return [ledZepplin, pearlJam, fooFighters];
-    }
+    var yellowLedbetter = Song.create({
+      title: 'Yellow Ledbetter',
+      band: 'Pearl Jam',
+      rating: 4
+    });
+
+    var pretender = Song.create({
+      title: 'The Pretender',
+      band: 'Foo Fighters',
+      rating: 2
+    });
+
+    var daughter = Song.create({
+      title: 'Daughter',
+      band: 'Pearl Jam',
+      rating: 5
+    });
+
+    const ledZepplin = Band.create({
+      name: 'Led Zepplin',
+      songs: [blackDog]
+    });
+
+    const pearlJam = Band.create({
+      name: 'Pearl Jam',
+      songs: [yellowLedbetter, daughter]
+    });
+
+    const fooFighters = Band.create({
+      name: 'Foo Fighter',
+      songs: [pretender]
+    });
+
+    return [ledZepplin, pearlJam, fooFighters];
+  }
 });
